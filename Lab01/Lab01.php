d1dba5ecd1458671f1a9138b0cfc21d541f57fc4
Your Name <you@example.com>
2023-07-28 09:50:07 +0700

    updata

---
+++ Lab01/Lab01.php
@@ -0,0 +1,22 @@
+<?php
+echo "Hello <br>";
+echo ("Hello <br>");
+echo "Hello World <br>";
+echo "hello","World";
+?>
+<hr>
+<?php
+$str = "hello string";
+$x =200;
+$y = 44.6;
+echo "string is: $str<br/>";
+echo "interger is: $x<br/>";
+echo "float is: $y<br/>";
+?>
+<hr>
+<?php
+$color = "red";
+echo "My car is " .$color."<br/>";
+echo "My hone is " .$color."<br/>";
+echo "My boat is " .$color."<br/>";
+?>
---
+++ Lab02/Lab02.php
@@ -0,0 +1,31 @@
+<?php
+$color = array( 'red', 'green', 'white');
+print_r($color);
+echo "<br>";
+echo $color [0];
+$age = array();
+$age [0] = 10;
+$age [1] = 20;
+$age [3] = 30;
+print_r($age);
+?>
+<hr>
+
+<?php
+$salaries = array(
+     "mohammad"> 2000,
+      "qadir" > 1000,
+       "zara" => 500
+        );
+echo "Salary of mohammad is". $salaries[ 'mohammad']. "<br />";
+echo "Salary of qadir is ". $salaries['qadir']. "<br />";
+echo "Salary of zara is ". $salaries ['zara']. "<br />";
+
+?>
+<hr>
+<?php
+define('LOCATOR', "/locator");
+define('CLASSES', LOCATOR."/code/classes");
+define('FUNCTIONS', LOCATOR. "/code/functions") ;
+define('USERDIR', LOCATOR."/user"); 
+?>
